0d11a1053ff3b7872baf16402cc4d0e3660b3062
Bumindu Yasith <bumiduyasith@gmail.com>
2022-01-15 00:34:52 +0530

    Client Api Service implemented

---
+++ BlazorWasmApp/Client/Services/SuperHeros/ISuperHeroApiService.cs
@@ -0,0 +1,9 @@
+﻿using BlazorWasmApp.Shared;
+
+namespace BlazorWasmApp.Client.Services.SuperHeros
+{
+    public interface ISuperHeroApiService
+    {
+       Task<List<SuperHero>> GetAllSuperHeros();
+    }
+}
---
+++ BlazorWasmApp/Client/Services/SuperHeros/SuperHeroApiService.cs
@@ -0,0 +1,19 @@
+﻿using BlazorWasmApp.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorWasmApp.Client.Services.SuperHeros
+{
+    public class SuperHeroApiService : ISuperHeroApiService
+    {
+        public readonly HttpClient _httpClient;
+        public SuperHeroApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<SuperHero>> GetAllSuperHeros()
+        {
+            var data =  await _httpClient.GetFromJsonAsync<List<SuperHero>>("api/superheros");
+            return data;
+        }
+    }
+}
